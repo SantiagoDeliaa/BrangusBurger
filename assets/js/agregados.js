9ca7cc61e96b381af32f6b375525875bcd4a019a
agustinnarciande <narciande.2019@gmail.com>
2022-11-02 14:14:33 -0300

    Agregados renderizados

--- assets/js/agregados.js
+++ assets/js/agregados.js
@@ -17,20 +17,20 @@ let products = await res.json()
 
 
 // Extraemos la variable con la categoria filtrada
-let extras = products.filter(el => el.categoria == "agregados")
+let extras = products.filter(el => el.categoria == "extras")
 let cervezas = products.filter(el => el.categoria == "cervezas")
 let sinAlcohol = products.filter(el => el.categoria == "sinAlcohol")
 
 
 // Renderizamos las cervezas en su respectivo contenedor
-cervezas.forEach( el => {
+cervezas.forEach(el => {
     cervezasCont.innerHTML += `
     <div class="agregados-row">
         <img class="agregados-image" src="${el.imagen}" width="100" max-height="70px">
             <span class="agregados-title">${el.nombre}</span>
-            <p>${el.descripcion}</p>
+            <span class="agregados-descripcion agregados-column">${el.desc}</span>
         <span class="agregados-price agregados-column">$${el.precio}</span>
-        <div class="agregados-cantidad agregados-column">
+        <div class=" agregados-column">
             <button class="btn btn-warning" type="button">Agregar</button>
         </div>
     </div>
@@ -38,37 +38,32 @@ cervezas.forEach( el => {
 });
 
 // Renderizamos las bebidas sin alcohol en su respectivo contenedor
-sinAlcohol.forEach( el => {
+sinAlcohol.forEach(el => {
     bebidasSinAlcoholCont.innerHTML += `
     <div class="agregados-row">
         <img class="agregados-image" src="${el.imagen}" width="100" max-height="70px">
             <span class="agregados-title">${el.nombre}</span>
             <span class="agregados-descripcion agregados-column">${el.desc}</span>
         <span class="agregados-price agregados-column">$${el.precio}</span>
-        <div class="agregados-cantidad agregados-column">
+        <div class=" agregados-column">
+            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#staticBackdrop" type="button">Agregar</button>
+        </div>
+    </div> 
+    `;
+});
+
+// Renderizamos los extras en su respectivo contenedor 
+extras.forEach( el => {
+    extrasCont.innerHTML += `
+    <div class="agregados-row">
+        <img class="agregados-image" src="${el.imagen}" width="100" max-height="70px">
+        <span class="agregados-title">${el.nombre}</span>
+        <span class="agregados-descripcion agregados-column">${el.desc}</span>
+        <span class="agregados-price agregados-column">$${el.precio}</span>
+        <div class=" agregados-column">
             <button class="btn btn-warning" type="button">Agregar</button>
         </div>
     </div>
     `;
 });
 
-// Renderizamos los extras en su respectivo contenedor 
-
-// extras.forEach( el => {
-//     extrasCont.innerHTML += `
-//     <div class="agregados-row">
-//         <div class="agregados-item agregados-column">
-//             <img class="agregados-item-image" src="${el.imagen}" width="100" max-height="70px">
-//             <span class="agregados-item-title">${el.nombre}</span>
-//         </div>
-//         <span class="agregados-price agregados-column">$${el.precio}</span>
-//         <div class="agregados-cantidad agregados-column">
-//             <input class="agregados-cantidad-input" min="1" max="4" type="number" value="1">
-//             <button class="btn btn-warning" type="button">Agregar</button>
-//         </div>
-//     </div>
-//     `;
-// });
-
-console.log(cervezas)
-console.log(sinAlcohol)
