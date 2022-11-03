a6eb28cefa632b6de94e361b214f923712353059
Delgado Sebastian <sebdelgado83@gmail.com>
2022-11-03 14:39:28 -0300

    mejora boton editar

--- assets/js/editar.js
+++ assets/js/editar.js
@@ -1,7 +1,7 @@
-const fetchStorageCarrito   = () => JSON.parse(localStorage.getItem('producto'));
+const fetchStorageProducto   = () => JSON.parse(localStorage.getItem('producto'));
 const editar = document.querySelector('.editar')
 
-console.log(fetchStorageCarrito()[0].nombre)
+console.log(fetchStorageProducto()[0].nombre)
 
 
 const card = document.createElement('div')
@@ -11,22 +11,22 @@ const card = document.createElement('div')
         <form action="http://localhost:8080/admin/?_method=PUT" method="post" >
             <div>
                 <label for="id" class="form-label">Id</label>
-                <input type="text" class="form-control" id="id" name="id" value="${fetchStorageCarrito()[0]._id}">
+                <input type="text" class="form-control" id="id" name="id" value="${fetchStorageProducto()[0]._id}">
             </div>
             <div>
                 <label for="nombre" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="nombre" name="nombre" value="${fetchStorageCarrito()[0].nombre}"> 
+                <input type="text" class="form-control" id="nombre" name="nombre" value="${fetchStorageProducto()[0].nombre}"> 
             </div>
             <div>
                 <label for="desc" class="form-label">desc</label>
-                <textarea type="text" class="form-control" id="desc" name="desc"> "${fetchStorageCarrito()[0].desc}"</textarea>
+                <textarea type="text" class="form-control" id="desc" name="desc"> "${fetchStorageProducto()[0].desc}"</textarea>
             </div>
             <div>
                 <label for="precio" class="form-label">precio</label>
-                <input type="number" class="form-control" id="precio" name="precio" value="${fetchStorageCarrito()[0].precio}">
+                <input type="number" class="form-control" id="precio" name="precio" value="${fetchStorageProducto()[0].precio}">
             </div>
             <div>
-                <img src="${fetchStorageCarrito()[0].imagen.substring(1)}" alt="">
+                <img src="${fetchStorageProducto()[0].imagen.substring(1)}" alt="">
             </div>
             <div>
                 <label for="imagen" class="form-label">Imagen</label>
